164deff6355d29cb279b23d5e7fbc1159da407b3
huangyu <huangyu@distrii.com>
2019-10-30 15:58:43 +0800

    循环队列实现 测试代码

---
+++ Queue/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+/**
+ * @author huangyu
+ * @version 1.0
+ * @date 2019/10/30 15:27
+ */
+public class Main {
+
+    private static double testQueue(Queue<Integer> q, int opCount){
+
+        long starTime = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            q.enqueue(random.nextInt(Integer.MAX_VALUE));
+        }
+        for (int i = 0; i < opCount; i++) {
+            q.dequeue();
+        }
+
+        long endTime = System.nanoTime();
+        return (endTime - starTime) / 1000000000.0;
+    }
+
+    public static void main(String[] args) {
+        int opCount = 1000000;
+
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time1 = testQueue(arrayQueue, opCount);
+        System.out.println("ArrayQueue, time: " + time1 + " s");
+
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        double time2 = testQueue(loopQueue, opCount);
+        System.out.println("LoopQueue, time: " + time2 + " s");
+    }
+
+}
